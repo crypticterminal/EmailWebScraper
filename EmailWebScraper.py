6e2cd2154c5725cfacfa74de2b9804e9026617bc
rafa <rafaeldechella@gmail.com>
2017-05-24 15:32:40 +0000

    It extracts links and email addresses recursively but it will go on infinitely if not stoped Next phase is entering a certain depth

--- EmailWebScraper.py
+++ EmailWebScraper.py
@@ -1,12 +1,62 @@
 import urllib2
-import re 
+import re
 from pprint import pprint
+import bs4
+from bs4 import BeautifulSoup
+from urlparse import urljoin
+import argparse
 
-url = "https://en.wikipedia.org/wiki/Email_address#Examples"
 
-response = urllib2.urlopen(url)
-webContent = response.read()
+parser = argparse.ArgumentParser(description='Takes out email addresses from '
+                                             'a website recursively')
+
+parser.add_argument('URL', metavar='URL', type=str, help='URL you want'
+                                                         ' to scrape')
+
+args = parser.parse_args()
 
 email_regex = re.compile(r"[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+")
 
-pprint(re.findall(email_regex, webContent))
+totallinks = []
+totalmails = set()
+
+
+def ExtraeCorreos(url):
+
+    webContent = urllib2.urlopen(url).read()
+
+    mails = re.findall(email_regex, webContent)
+
+    totalmails.update(mails)
+
+    return len(mails)
+
+
+def ExtraeLinks(url):
+
+    webContent = urllib2.urlopen(url).read()
+
+    sopa = BeautifulSoup(webContent, 'html.parser')
+
+    links = sopa.find_all('a', href=True)
+
+    for link in links:
+        if link['href'].startswith('http'):
+            totallinks.append(link['href'])
+        elif not link['href'].startswith('http'):
+            totallinks.append(urljoin(url, link['href']))
+
+    return len(links)
+
+
+url = "http://192.168.186.8"
+totallinks.append(url)
+for link in totallinks:
+
+    correosenlink = ExtraeCorreos(link)
+    linksenlink = ExtraeLinks(link)
+    print "En %s hay %s correos y %s links " % (link, correosenlink,
+                                                linksenlink)
+
+pprint(totalmails)
+print len(totalmails)
