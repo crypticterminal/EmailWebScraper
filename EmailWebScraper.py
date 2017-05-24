cbf186032430c8a49c32d3f670ba4dbdcb0570e9
rafa <rafaeldechella@gmail.com>
2017-05-24 17:25:51 +0000

    Not there can't be duplicated url in the search

--- EmailWebScraper.py
+++ EmailWebScraper.py
@@ -3,7 +3,7 @@ import re
 from pprint import pprint
 import bs4
 from bs4 import BeautifulSoup
-from urlparse import urljoin
+from urlparse import urljoin, urlparse
 import argparse
 
 
@@ -19,9 +19,10 @@ email_regex = re.compile(r"[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+")
 
 totallinks = []
 totalmails = set()
-
+controllinks = set()
 
 def ExtraeCorreos(url):
+    
     try:
         webContent = urllib2.urlopen(url).read()
 
@@ -30,11 +31,14 @@ def ExtraeCorreos(url):
         totalmails.update(mails)
 
         return len(mails)
+
     except urllib2.HTTPError, e:
+
         print e
 
 
 def ExtraeLinks(url):
+
     try:
         webContent = urllib2.urlopen(url).read()
 
@@ -43,22 +47,41 @@ def ExtraeLinks(url):
         links = sopa.find_all('a', href=True)
 
         for link in links:
-            if link['href'].startswith('http'):
-                totallinks.append(link['href'])
-            elif not link['href'].startswith('http'):
-                totallinks.append(urljoin(url, link['href']))
+            link = link['href']
+            absolute = bool(link.startswith('http'))
+            
+            if absolute:
+
+                if link in controllinks:
+                    pass
+                else:
+                    totallinks.append(link)
+                    controllinks.add(link)
+
+            elif not absolute:
+
+                unitedurl = urljoin(url, link)
+
+                if unitedurl in controllinks:
+                    pass
+                else:
+                    totallinks.append(unitedurl)
+                    controllinks.add(unitedurl)
 
         return len(links)
 
     except urllib2.HTTPError, e:
+
         print e
 
 url = "http://%s" % args.URL
 totallinks.append(url)
 
+domain = urlparse(url)
+
 for link in totallinks:
 
-    if len(totalmails) > 10:
+    if len(totalmails) > 10 or urlparse(link).netloc != domain.netloc: #asi solo se queda en el dominio, ponerlo como opcion en command line
         break
     correosenlink = ExtraeCorreos(link)
     linksenlink = ExtraeLinks(link)
