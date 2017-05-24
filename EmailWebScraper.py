e66d54e3de7b34823a7e7c444817507eeb96c02d
rafa <rafaeldechella@gmail.com>
2017-05-24 16:13:03 +0000

    It stops when extracting 10 addresses, im going to add number of addresses as optional argument, also add depth as argument, also add stop when exiting domain option

--- EmailWebScraper.py
+++ EmailWebScraper.py
@@ -7,14 +7,14 @@ from urlparse import urljoin
 import argparse
 
 
-parser = argparse.ArgumentParser(description='Takes out email addresses from '
+parser = argparse.ArgumentParser(description='Scrapes email addresses from '
                                              'a website recursively')
 
 parser.add_argument('URL', metavar='URL', type=str, help='URL you want'
                                                          ' to scrape')
 
 args = parser.parse_args()
-
+print args
 email_regex = re.compile(r"[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+")
 
 totallinks = []
@@ -22,37 +22,44 @@ totalmails = set()
 
 
 def ExtraeCorreos(url):
+    try:
+        webContent = urllib2.urlopen(url).read()
 
-    webContent = urllib2.urlopen(url).read()
+        mails = re.findall(email_regex, webContent)
 
-    mails = re.findall(email_regex, webContent)
+        totalmails.update(mails)
 
-    totalmails.update(mails)
-
-    return len(mails)
+        return len(mails)
+    except urllib2.HTTPError, e:
+        print e
 
 
 def ExtraeLinks(url):
+    try:
+        webContent = urllib2.urlopen(url).read()
 
-    webContent = urllib2.urlopen(url).read()
+        sopa = BeautifulSoup(webContent, 'html.parser')
 
-    sopa = BeautifulSoup(webContent, 'html.parser')
+        links = sopa.find_all('a', href=True)
 
-    links = sopa.find_all('a', href=True)
+        for link in links:
+            if link['href'].startswith('http'):
+                totallinks.append(link['href'])
+            elif not link['href'].startswith('http'):
+                totallinks.append(urljoin(url, link['href']))
 
-    for link in links:
-        if link['href'].startswith('http'):
-            totallinks.append(link['href'])
-        elif not link['href'].startswith('http'):
-            totallinks.append(urljoin(url, link['href']))
+        return len(links)
 
-    return len(links)
+    except urllib2.HTTPError, e:
+        print e
 
-
-url = "http://192.168.186.8"
+url = "http://%s" % args.URL
 totallinks.append(url)
+
 for link in totallinks:
 
+    if len(totalmails) > 10:
+        break
     correosenlink = ExtraeCorreos(link)
     linksenlink = ExtraeLinks(link)
     print "En %s hay %s correos y %s links " % (link, correosenlink,
